de4a85367e41a213041a8616586e06695d5fc936
Abhijit Naik <naikabhiit@gmail.com>
2023-10-26 14:16:33 +0530

    NoteApp: Initial commit with dependencies added in build.gradle file
    Signed-off-by: Abhijit Naik <naikabhiit@gmail.com>

---
+++ noteapp/app/src/main/java/com/compose/noteapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.compose.noteapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.compose.noteapp.ui.theme.NoteAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NoteAppTheme {
+
+            }
+        }
+    }
+}
